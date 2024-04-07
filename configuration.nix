fa7d14658bfcd4fd873595ee6ccb48a3a423f6e0
Alexis Lapierre <128792625+Alexis-Lapierre@users.noreply.github.com>
2024-04-07 15:01:36 +0200

    First commited version of configuration.nix
    Signed-off-by: Alexis Lapierre <68123353+Helkindown@users.noreply.github.com>

---
+++ configuration.nix
@@ -0,0 +1,175 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+  let
+    unstable = import <nixos-unstable> { config = config.nixpkgs.config; };
+    username = "alexis_lapierre";
+  in {
+  imports =
+    [ # Include the results of the hardware scan.
+      /etc/nixos/hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "NixOS"; # Define your hostname.
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Paris";
+
+  # Locale
+  i18n = {
+    defaultLocale = "en_GB.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "fr_FR.UTF-8";
+      LC_IDENTIFICATION = "fr_FR.UTF-8";
+      LC_MEASUREMENT = "fr_FR.UTF-8";
+      LC_MONETARY = "fr_FR.UTF-8";
+      LC_NAME = "fr_FR.UTF-8";
+      LC_NUMERIC = "fr_FR.UTF-8";
+      LC_PAPER = "fr_FR.UTF-8";
+      LC_TELEPHONE = "fr_FR.UTF-8";
+      LC_TIME = "fr_FR.UTF-8";
+    };
+  };
+
+  # Configure keymap in X11
+  services.xserver = {
+    enable = true;
+    layout = "fr";
+    xkbVariant = "us";
+    xkbOptions = "caps:swapescape";
+
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+
+    # do not use the xterm terminal
+    excludePackages = [ pkgs.xterm ]; 
+  };
+
+  # enable auto login for main user + workaround found here:
+  # https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+  services.xserver.displayManager.autoLogin = {
+    enable = true;
+    user = username;
+  };
+
+  # Configure console keymap
+  console.keyMap = "us";
+
+  # No need for printer here.
+  services.printing.enable = false;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  users.mutableUsers = false;
+  users.users.${username} = {
+    # mutableUsers specific part
+    uid = 1000;
+    hashedPasswordFile = "/home/${username}/nix/password";
+
+    shell = pkgs.fish;
+    isNormalUser = true;
+    description = "Alexis Lapierre";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      # Web browsers I use often
+      firefox
+      brave
+
+      # Gnome needs gnome tweaks to access some settings
+      gnome.gnome-tweaks
+
+      # My prefered terminal emulator
+      unstable.kitty
+
+      # clipboard for helix
+      wl-clipboard
+
+      # My programming needs 
+      git
+      gnupg
+      unstable.gitui
+      androidStudioPackages.canary
+
+      # tty commands I use
+      unstable.eza
+      unstable.zoxide
+      bat
+      neofetch
+
+      # Communication with friends
+      discord
+      telegram-desktop
+
+      # Gaming!
+      # run (in this case) visual novels via steam run
+      steam-run
+      # Other random video games I have collected require wine
+      # Force wayland with wine 32 bits because I like wayland
+      wineWowPackages.waylandFull
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # allow me to use nix command directly
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  environment = {
+    gnome.excludePackages = with pkgs; [
+      geany # I use helix 
+      gnome.epiphany # I use firefox
+      gnome.geary # No need for an email client
+      gnome.gnome-terminal # I use kitty
+    ];
+
+    # do not use gnome terminal or epiphany (web browser)
+    systemPackages = with pkgs; [
+      unstable.helix # My editor, latest version
+      nil # Nix language server, used by helix
+
+      # Some tools I use often
+      htop
+      file # standard utils tool
+      ethtool
+      killall
+    ];
+
+    # Since there is no programs.helix.defaultEditor, just plainly force this value for everyone
+    # A redditor seemed really mad about this
+    variables.EDITOR = "hx";
+  };
+
+  programs.fish.enable = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+}
