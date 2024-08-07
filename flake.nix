861c872d36377048164dc5d2ad0eccc6e23b22f2
Alexis Lapierre <128792625+Alexis-Lapierre@users.noreply.github.com>
2024-08-07 22:05:30 +0200

    Try out cosmic-epoch on CirnOS

--- flake.nix
+++ flake.nix
@@ -4,33 +4,43 @@
   inputs = {
     stable.url = "nixpkgs/nixos-24.05";
     unstable.url = "nixpkgs/nixos-unstable";
+    nixos-cosmic = {
+      url = "github:lilyinstarlight/nixos-cosmic";
+      inputs.nixpkgs.follows = "unstable";
+    };
   };
-
-  outputs = { self, stable, unstable }:
+  outputs = { self, stable, unstable, nixos-cosmic }:
     {
-      nixosConfigurations = {
+      nixosConfigurations = let 
+        makeModule = baseModule : [
+          {
+            nix.settings = {
+              substituters = [ "https://cosmic.cachix.org/" ];
+              trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+            };
+          }
+          nixos-cosmic.nixosModules.default
+          baseModule
+        ];
+        makeSpecialArgs = system : {
+          unstable = import unstable {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        };
+       in  {
         CirnOS = stable.lib.nixosSystem rec {
           system = "x86_64-linux";
-          modules = [ ./devices/CirnOS/configuration.nix ];
+          modules = makeModule ./devices/CirnOS/configuration.nix;
 
-          specialArgs = {
-            unstable = import unstable {
-              inherit system;
-              config.allowUnfree = true;
-            };
-          };
+          specialArgs = makeSpecialArgs system;
         };
 
         MomBasement = stable.lib.nixosSystem rec {
           system = "x86_64-linux";
-          modules = [ ./devices/MomBasement/configuration.nix ];
+          modules = makeModule ./devices/MomBasement/configuration.nix;
 
-          specialArgs = {
-            unstable = import unstable {
-              inherit system;
-              config.allowUnfree = true;
-            };
-          };
+          specialArgs = makeSpecialArgs system;
         };
       };
     };
