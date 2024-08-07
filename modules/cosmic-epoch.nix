6c974d4e38ded8ec967b7b669bc8cb9c505d31b6
Alexis Lapierre <128792625+Alexis-Lapierre@users.noreply.github.com>
2024-08-07 22:43:37 +0200

    Add wofi to cosmic module

--- modules/cosmic-epoch.nix
+++ modules/cosmic-epoch.nix
@@ -1,7 +1,12 @@
-{ }:
+{ pkgs, username }:
 {
   services = {
     desktopManager.cosmic.enable = true;
     displayManager.cosmic-greeter.enable = true;
+  };
+  users.users.${username} = {
+    packages = with pkgs; [
+      wofi # Used in a shortcut
+    ];
   };
 }
