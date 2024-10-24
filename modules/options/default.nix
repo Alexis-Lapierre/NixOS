eb4baf9843731fbc425b61732c31272502ad74e7
Alexis Lapierre <128792625+Alexis-Lapierre@users.noreply.github.com>
2024-10-25 00:02:23 +0200

    Put PocketWizard custom layout into an option

--- modules/options/default.nix
+++ modules/options/default.nix
@@ -3,5 +3,6 @@
   imports = [
       ./cosmicEpoch.nix
       ./games.nix
+      ./layoutEurKeyDown.nix
   ];
 }
---
+++ modules/options/layoutEurKeyDown.nix
@@ -0,0 +1,15 @@
+{ lib, config, ... }:
+{
+  options.AlexisLapierre.eurkey_down.enable = lib.mkEnableOption ''
+    Create additional keyboard layout: EurKey Down.
+    Eurokey layer 3 and 4 (accesible with AltGr) shifted down to layer 1 and 2.
+  '';
+
+  config = lib.mkIf config.AlexisLapierre.eurkey_down.enable {
+    services.xserver.xkb.extraLayouts.eurkey_down = {
+      description = "Eurkey layout shifted down - layer 3 and 4 are moved down to layer 1 and 2.";
+      languages = [ "eng" "fr" ];
+      symbolsFile = ./layout/symbols/eurkey_down;
+    };
+  };
+}
