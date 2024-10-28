386c38544526f994c133346e03bc915cdb520f6b
Alexis Lapierre <128792625+Alexis-Lapierre@users.noreply.github.com>
2024-10-28 12:54:25 +0100

    Homemanager: customize fish prompt

--- modules/homemanager/home.nix
+++ modules/homemanager/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }: {
+{ ... }: {
   home.username = "cirno";
   home.homeDirectory = "/home/cirno";
   home.stateVersion = "24.05";
@@ -33,6 +33,31 @@
       ssh = "kitty +kitten ssh";
       cat = "bat";
     };
+
+    functions.fish_prompt = ''
+      #Save the return status of the previous command
+      set -l last_pipestatus $pipestatus
+      set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+
+      if functions -q fish_is_root_user; and fish_is_root_user
+          printf '%s@%s %s%s%s# ' $USER (prompt_hostname) (set -q fish_color_cwd_root
+                                                           and set_color $fish_color_cwd_root
+                                                           or set_color $fish_color_cwd) \
+              (prompt_pwd) (set_color normal)
+      else
+          set -l status_color (set_color $fish_color_status)
+          set -l statusb_color (set_color --bold $fish_color_status)
+          set -l pipestatus_string (__fish_print_pipestatus [ ] \| "$status_color" "$statusb_color" $last_pipestatus)
+
+          printf '[%s] %s@%s %s %s %s\n> ' \
+              (date "+%H:%M:%S") \
+              (set_color aa1fef)$USER(set_color white) \
+              (set_color 1fefaa)(prompt_hostname)(set_color efaa1f) \
+              (prompt_pwd) \
+              $pipestatus_string \
+              (set_color normal)
+      end
+    '';
   };
 
   programs.zoxide = {
